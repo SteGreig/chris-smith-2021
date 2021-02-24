01040630afbfd26fc351ed23cf6d20a6483d7973
Stephen Greig <stephengreig@Stephens-MacBook-Pro.local>
2021-02-24 23:13:45 +0000

    Couple of fixes for ios

--- wp-content/themes/chris-smith/_resources/js/run.js
+++ wp-content/themes/chris-smith/_resources/js/run.js
@@ -5,11 +5,23 @@
 	// add all of your code within here, not above or below
 	$(function () {
 
+
+			// Accordion
 			$('.faq-q').click(function() {
 				$(this).toggleClass('active');
 				$(this).next('.faq-a').slideToggle();
 				$(this).find('svg').toggle();
 			});
+
+
+			// Smooth scroll for anchor links
+			$(document).on('click', 'a[href^="#"]', function (event) {
+				event.preventDefault();
+		
+				$('html, body').animate({
+						scrollTop: $($.attr(this, 'href')).offset().top
+				}, 500);
+		});
 
 	});
 
