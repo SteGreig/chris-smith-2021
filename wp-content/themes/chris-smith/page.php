ed04d6a369098d0fe5f3b427957b77fbf0250bed
Stephen Greig <stephengreig@Stephens-MacBook-Pro.local>
2021-03-24 00:21:55 +0000

    Fix for dynamic sidebar issue

--- wp-content/themes/chris-smith/page.php
+++ wp-content/themes/chris-smith/page.php
@@ -4,4 +4,8 @@ $context = Timber::context();
 $timber_post = new Timber\Post();
 $context['post'] = $timber_post;
 
+if(is_page('reviews')) {
+  $context['dynamic_sidebar'] = Timber::get_widgets('reviews_page');
+}
+
 Timber::render( [ 'page-'.$timber_post->slug.'.twig', 'page.twig' ], $context );
